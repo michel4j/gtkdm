ccfcacc2664b84936f84bdd8787d071196127638
Michel Fodje <michel.fodje@lightsource.ca>
2019-05-14 15:44:37 -0600

    Align pixels for cairo drawing to get sharp edges

--- gtkdm/widgets.py
+++ gtkdm/widgets.py
@@ -6,11 +6,19 @@ from gi.repository import Gtk, GObject, Gdk, Gio
 import gepics
 
 
+def pix(v):
+    """Round to neareast 0.5 for cairo drawing"""
+    x = round(v*2)
+    return x/2 if x%2 else (x+1)/2
+
+
 class Display(Gtk.Fixed):
     __gtype_name__ = 'Display'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+Direction = Gdk.WindowEdge
 
 
 class TextMonitor(Gtk.EventBox):
@@ -80,7 +88,6 @@ class TextLabel(Gtk.EventBox):
         self.get_style_context().add_class('gtkdm')
 
 
-Direction = Gdk.WindowEdge
 
 
 class LineMonitor(Gtk.Widget):
@@ -113,7 +120,7 @@ class LineMonitor(Gtk.Widget):
         elif self.direction in [Direction.EAST, Direction.WEST]:
             y1 = y2 = allocation.height / 2
 
-        return x1, y1, x2, y2
+        return pix(x1), pix(y1), pix(x2), pix(y2)
 
     def do_draw(self, cr):
         # draw line
@@ -184,6 +191,7 @@ class Byte(Gtk.Widget):
         allocation = self.get_allocation()
         stride = ceil(8 / self.columns)
         col_width = allocation.width / self.columns
+
         # draw boxes
         if not self.color:
             self.props.color = Gdk.RGBA(red=0.0, green=1.0, blue=0.0, alpha=1.0)
@@ -192,14 +200,14 @@ class Byte(Gtk.Widget):
             border_color = Gdk.RGBA(red=0.0, green=0.0, blue=0.0, alpha=1.0)
         else:
             border_color = Gdk.RGBA(red=1.0, green=1.0, blue=1.0, alpha=1.0)
+        label_color = self.get_style_context().get_color(Gtk.StateFlags.NORMAL)
 
-        cr.set_line_width(0.5)
-        cr.set_font_size(9)
+        cr.set_line_width(1)
+        #cr.set_font_size(9)
 
         for i in range(8):
-            x = (i // stride) * col_width + 4
-
-            y = 4 + (i % stride) * 14
+            x = pix((i // stride) * col_width + 4)
+            y = pix(4 + (i % stride) * 14)
             cr.rectangle(x, y, 10, 10)
             if self._view_bits[i] == '1':
                 cr.set_source_rgba(*self.color)
@@ -209,6 +217,7 @@ class Byte(Gtk.Widget):
             cr.set_source_rgba(*border_color)
             cr.stroke()
 
+            cr.set_source_rgba(*label_color)
             label = self._view_labels[i]
             xb, yb, w, h = cr.text_extents(label)[:4]
             cr.move_to(x + 14, y + h - 1)
@@ -272,6 +281,99 @@ class Byte(Gtk.Widget):
         self.queue_draw()
 
 
+class Indicator(Gtk.Widget):
+    __gtype_name__ = 'Indicator'
+    channel = GObject.Property(type=str, default='', nick='PV Name')
+    label = GObject.Property(type=str, default='', nick='Label')
+    color = GObject.Property(type=Gdk.RGBA, nick='Color')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_size_request(196, 40)
+        self.box_color = self.color
+        self.pv = None
+        self.label_pv = None
+
+    def do_draw(self, cr):
+        cr.set_line_width(0.5)
+        x = 4.5
+        y = 4.5
+        label_color = self.get_style_context().get_color(Gtk.StateFlags.NORMAL)
+        if self.box_color:
+            cr.set_source_rgba(*self.box_color)
+
+        cr.rectangle(x, y, 10, 10)
+        cr.fill_preserve()
+        cr.set_source_rgba(*label_color)
+        cr.stroke()
+
+        xb, yb, w, h = cr.text_extents(self.label)[:4]
+        cr.move_to(x + 14, y + h - 1.5)
+        cr.show_text(self.label)
+
+    def do_realize(self):
+        allocation = self.get_allocation()
+        attr = Gdk.WindowAttr()
+        attr.window_type = Gdk.WindowType.CHILD
+        attr.x = allocation.x
+        attr.y = allocation.y
+        attr.width = allocation.width
+        attr.height = allocation.height
+        attr.visual = self.get_visual()
+        attr.event_mask = self.get_events() | Gdk.EventMask.EXPOSURE_MASK
+        WAT = Gdk.WindowAttributesType
+        mask = WAT.X | WAT.Y | WAT.VISUAL
+        window = Gdk.Window(self.get_parent_window(), attr, mask);
+        self.set_window(window)
+        self.register_window(window)
+        self.set_realized(True)
+        window.set_background_pattern(None)
+
+        pv_name = self.channel
+        if pv_name:
+            self.pv = gepics.PV(pv_name)
+            self.pv.connect('changed', self.on_change)
+            self.pv.connect('alarm', self.on_alarm)
+            self.pv.connect('active', self.on_active)
+
+            if not self.label:
+                self.label_pv = gepics.PV('{}.DESC'.format(pv_name))
+                self.label_pv.connect('changed', self.on_label_change)
+
+    def on_label_change(self, pv, value):
+        self.props.label = value
+        self.queue_draw()
+
+    def on_change(self, pv, value):
+        if value:
+            self.box_color = Gdk.RGBA(red=0.0, green=1.0, blue=0.0, alpha=1.0)
+        else:
+            self.box_color = Gdk.RGBA(red=0.5, green=0.5, blue=0.5, alpha=1.0)
+        self.queue_draw()
+
+    def on_alarm(self, pv, alarm):
+        if alarm == gepics.Alarm.CRITICAL:
+            self.get_style_context().remove_class('gtkdm-warning')
+            self.get_style_context().add_class('gtkdm-critical')
+        elif alarm == gepics.Alarm.WARNING:
+            self.get_style_context().add_class('gtkdm-warning')
+            self.get_style_context().remove_class('gtkdm-critical')
+        else:
+            self.get_style_context().remove_class('gtkdm-warning')
+            self.get_style_context().remove_class('gtkdm-critical')
+
+    def on_active(self, pv, connected):
+        if connected:
+            self.pv.get_with_metadata()
+            self.get_style_context().remove_class('gtkdm-inactive')
+            self.box_color = Gdk.RGBA(red=0.0, green=1.0, blue=0.0, alpha=0.0)
+        else:
+            self.get_style_context().add_class('gtkdm-inactive')
+            self.box_color = Gdk.RGBA(red=1.0, green=1.0, blue=1.0, alpha=1.0)
+
+        self.queue_draw()
+
+
 class ScaleControl(Gtk.EventBox):
     __gtype_name__ = 'ScaleControl'
     channel = GObject.Property(type=str, default='', nick='PV Name')
@@ -297,14 +399,11 @@ class ScaleControl(Gtk.EventBox):
         self.get_style_context().add_class('gtkdm')
         self.adjustment.configure(0.0, self.minimum, self.maximum, self.increment, 0, 0)
 
-        if self.orientation == Gtk.Orientation.HORIZONTAL:
-            position = Gtk.PositionType.TOP
-        else:
-            position = Gtk.PositionType.LEFT
+        position = Gtk.PositionType.TOP if self.orientation == Gtk.Orientation.HORIZONTAL else Gtk.PositionType.LEFT
 
         self.scale.clear_marks()
-        self.scale.add_mark(self.minimum, Gtk.PositionType.TOP, '{}'.format(self.minimum))
-        self.scale.add_mark(self.maximum, Gtk.PositionType.TOP, '{}'.format(self.maximum))
+        self.scale.add_mark(self.minimum, position, '{}'.format(self.minimum))
+        self.scale.add_mark(self.maximum, position, '{}'.format(self.maximum))
 
         pv_name = self.channel
         if pv_name:
